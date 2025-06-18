52f502db7577bf0ceb93fc39dd93c714aab4093a
Hong Son <hongson530.dev@gmail.com>
2025-06-19 01:06:16 +0700

    init: init backend structure

---
+++ restaurant-backend/src/test/java/com/shongon/restaurant_backend/RestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shongon.restaurant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
