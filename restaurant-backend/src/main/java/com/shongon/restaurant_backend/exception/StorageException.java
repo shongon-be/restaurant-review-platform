7a1caa798f1dd49f27c2538f9a196b2eff766279
Hong Son <hongson530.dev@gmail.com>
2025-06-20 17:26:29 +0700

    exception: created Base & Storage exceptions

---
+++ restaurant-backend/src/main/java/com/shongon/restaurant_backend/exception/StorageException.java
@@ -0,0 +1,19 @@
+package com.shongon.restaurant_backend.exception;
+
+public class StorageException extends BaseException {
+
+  public StorageException() {
+  }
+
+  public StorageException(String message) {
+    super(message);
+  }
+
+  public StorageException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public StorageException(Throwable cause) {
+    super(cause);
+  }
+}
