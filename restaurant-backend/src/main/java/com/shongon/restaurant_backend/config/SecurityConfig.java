27d525b5bca8825866b6b5cabc6caa4ed6be6b88
Hong Son <hongson530.dev@gmail.com>
2025-06-21 17:12:41 +0700

    feat - Photo: created retrieve photo endpoint

--- restaurant-backend/src/main/java/com/shongon/restaurant_backend/config/SecurityConfig.java
+++ restaurant-backend/src/main/java/com/shongon/restaurant_backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.shongon.restaurant_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
--- restaurant-backend/src/main/java/com/shongon/restaurant_backend/controller/PhotoController.java
+++ restaurant-backend/src/main/java/com/shongon/restaurant_backend/controller/PhotoController.java
@@ -5,6 +5,11 @@ import com.shongon.restaurant_backend.domain.entities.Photo;
 import com.shongon.restaurant_backend.mapper.PhotoMapper;
 import com.shongon.restaurant_backend.service.blueprint.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,9 +21,23 @@ public class PhotoController {
     private final PhotoService photoService;
     private final PhotoMapper photoMapper;
 
-    @PostMapping()
+    @PostMapping
     public PhotoDTO uploadPhoto(@RequestParam("file") MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDTO(savedPhoto);
     }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.loadPhotoAsResource(id)
+                .map(photo -> ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory.getMediaType(photo)
+                                .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+                ).orElse(ResponseEntity.notFound().build());
+    }
+
 }
