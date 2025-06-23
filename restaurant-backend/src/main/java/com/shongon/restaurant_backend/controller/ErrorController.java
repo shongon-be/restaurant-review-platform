4418463592a8ea9bff65ffbf19f89134f535197d
Hong Son <hongson530.dev@gmail.com>
2025-06-23 16:46:21 +0700

    exception - Restaurant: handling MethodArgumentNotValidException (create restaurant)

--- restaurant-backend/src/main/java/com/shongon/restaurant_backend/controller/ErrorController.java
+++ restaurant-backend/src/main/java/com/shongon/restaurant_backend/controller/ErrorController.java
@@ -6,14 +6,35 @@ import com.shongon.restaurant_backend.exception.StorageException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @ControllerAdvice
 @Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        log.error("Caught MethodArgumentNotValidException: {}",e.getMessage());
+
+        String errorMessage = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorDTO errorDTO = ErrorDTO.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMessage)
+                .build();
+
+        return new ResponseEntity<>(errorDTO,HttpStatus.BAD_REQUEST);
+    }
 
     // Handle storage-related exceptions
     @ExceptionHandler(StorageException.class)
